f5cadaf084b68fa901dd4d36df1f933435bcee57
Mohamedeldfrawy <mohmmedeldfrawy@gmail.com>
2026-02-04 04:11:10 +0200

    Task3 - Javascript function and for loop

--- index.js
+++ index.js
@@ -1,81 +1,50 @@
 
 //Q1
-// var Name = window.prompt("plase enter any number");
-// console.log(Name);
+// var  sumNumber = 0;
+// function sum(number) {
+   
+//  for (var i = 1; i <= number+1; i++) {
+//         sumNumber += i; 
+        
+//     }
+//     console.log(sumNumber);   
+// }
+//  var number= Number(window.prompt("Enter a number:")); 
+// sum(number); 
 
-
-//--------------------------------------------------------
+//--------------------------------------------------------------
 //Q2
-// var num= window.prompt("enter number");
-// if (num%3==0 && num%4==0){
 
-// console.log("Yes");
 
-// }else{
-
-//     console.log("NO");
+//  var  SumNumberEven = 0;
+// function sum(number) {
+   
+//  for (var i = 1; i <= number+1; i++) {
+//     if(i%2==0){
+//         SumNumberEven ++; 
+        
+//     }
 // }
+//     console.log(SumNumberEven);   
 
-//-----------------------------------------------------
+// }
+//  var number= Number(window.prompt("Enter a number:")); 
+// sum(number); 
+
+//--------------------------------------------------------------
 //Q3
-
-// var  num1= Number(window.prompt("enter  the frist number"));
-// var num2= Number(window.prompt("enter  the second number"));
-
-// if ( num1>num2){
-
-//     console.log(num1);
-
-    
-// }else  {
-
-//    console.log(num2);
+//  var  SumNumberOdd = 0;
+// function sum(number) {
+   
+//  for (var i = 1; i <= number+1; i++) {
+//     if(i%2!=0){
+//         SumNumberOdd ++; 
+        
+//     }
 // }
-
-//----------------------------------------------------------
-//Q4
-
-// var  num1= Number(window.prompt("enter  the frist number"));
-
-// if ( num1>0){
-
-//     console.log("POSTIVE");
-
-    
-// }else if (num1==0)  {
-
-//    console.log("Zero");
-// }
-// else{
-
-//     console.log("Negative");
-// }
-
-//----------------------------------------------------------
-//Q5
-// var  num1= Number(window.prompt("enter  the frist number"));
-// var  num2= Number(window.prompt("enter  the frist number"));
-// var  num3= Number(window.prompt("enter  the three number"));
-
-// if (num1>num2 && num1>num3 && num2>num3){
-
-//     console.log(num1);
-//     console.log(num3);
-    
-// }
-//  else if (num2>num1 && num2>num3 && num1>num3){
-
-//     console.log(num2);
-//     console.log(num3);
-    
-// }
-// else{
-// console.log(num3);
-// console.log(num1);
+//     console.log(SumNumberOdd);   
 
 // }
-
-
-
-
+//  var number= Number(window.prompt("Enter a number:")); 
+// sum(number); 
 
